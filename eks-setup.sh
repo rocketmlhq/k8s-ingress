0e92bb742115bc626c9b5434add6bcfbbfe98814
Mayank Kothari <mayankkthr@gmail.com>
2022-02-17 19:00:31 +0530

    commit

--- eks-setup.sh
+++ eks-setup.sh
@@ -12,15 +12,16 @@ curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/
 && mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin \
 && kubectl version --short --client && rm kubectl
 
+aws eks update-kubeconfig --region $AWS_REGION --name $VALUE_1 \
+&& git clone https://github.com/mayankkthr/k8s-ingress.git
+
+sed -i "s/ClusterNameValue/$VALUE_1/" k8s-ingress/cluster-autoscaler.yaml
 sed -i "s/value1/$VALUE_3/" k8s-ingress/aws-auth-cm.yaml
 sed -i "s/value2/$VALUE_4/" k8s-ingress/aws-auth-cm.yaml
 sed -i "s/value3/$VALUE_5/" k8s-ingress/aws-auth-cm.yaml
 sed -i "s/value4/$VALUE_6/" k8s-ingress/aws-auth-cm.yaml
 
-kubectl apply -f k8s-ingress/aws-auth-cm.yaml
-
-aws eks update-kubeconfig --region $AWS_REGION --name $VALUE_1 \
-&& git clone https://github.com/mayankkthr/k8s-ingress.git \
+kubectl apply -f k8s-ingress/aws-auth-cm.yaml \
 && kubectl apply -f k8s-ingress/ns-and-sa.yaml \
 && kubectl apply -f k8s-ingress/default-server-secret.yaml \
 && kubectl apply -f k8s-ingress/nginx-config.yaml \
@@ -33,10 +34,7 @@ aws eks update-kubeconfig --region $AWS_REGION --name $VALUE_1 \
 && kubectl annotate serviceaccount cluster-autoscaler -n kube-system eks.amazonaws.com/role-arn=$VALUE_2 \
 && kubectl patch deployment cluster-autoscaler -n kube-system -p '{"spec":{"template":{"metadata":{"annotations":{"cluster-autoscaler.kubernetes.io/safe-to-evict": "false"}}}}}' \
 && kubectl set image deployment cluster-autoscaler -n kube-system cluster-autoscaler=k8s.gcr.io/autoscaling/cluster-autoscaler:v1.21.0 \
-&& kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
-
-sed -i "s/ClusterNameValue/$VALUE_1/" k8s-ingress/cluster-autoscaler.yaml
-
-kubectl apply -f k8s-ingress/cluster-autoscaler.yaml
+&& kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml \
+&& kubectl apply -f k8s-ingress/cluster-autoscaler.yaml
 
 rm -rf k8s-ingress/
