9c01059d49ebccc81e3c76f8e627b98e0cb94d34
Mayank Kothari <mayankkthr@gmail.com>
2022-02-17 21:08:30 +0530

    commit

--- eks-setup.sh
+++ eks-setup.sh
@@ -30,7 +30,6 @@ kubectl apply -f k8s-ingress/aws-auth-cm.yaml \
 && kubectl apply -f k8s-ingress/loadbalancer-aws-elb.yaml \
 && kubectl apply -f k8s-ingress/nginx-config-modified.yaml \
 && kubectl apply -f https://raw.githubusercontent.com/kubernetes/autoscaler/master/cluster-autoscaler/cloudprovider/aws/examples/cluster-autoscaler-autodiscover.yaml \
-&& kubectl annotate serviceaccount cluster-autoscaler -n kube-system eks.amazonaws.com/role-arn=$2 \
 && kubectl patch deployment cluster-autoscaler -n kube-system -p '{"spec":{"template":{"metadata":{"annotations":{"cluster-autoscaler.kubernetes.io/safe-to-evict": "false"}}}}}' \
 && kubectl set image deployment cluster-autoscaler -n kube-system cluster-autoscaler=k8s.gcr.io/autoscaling/cluster-autoscaler:v1.21.0 \
 && kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml \
