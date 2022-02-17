652709711a381903c3297fb9edf324cb4d37d34d
Mayank Kothari <mayankkthr@gmail.com>
2022-02-17 20:53:19 +0530

    commit

--- eks-setup.sh
+++ eks-setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 VALUE_1=$(printf '%s\n' "$1" | sed -e 's/[\/&]/\\&/g')
-VALUE_2=$(printf '%s\n' "$2" | sed -e 's/[\/&]/\\&/g')
 VALUE_3=$(printf '%s\n' "$3" | sed -e 's/[\/&]/\\&/g')
 VALUE_4=$(printf '%s\n' "$4" | sed -e 's/[\/&]/\\&/g')
 VALUE_5=$(printf '%s\n' "$5" | sed -e 's/[\/&]/\\&/g')
@@ -31,7 +30,7 @@ kubectl apply -f k8s-ingress/aws-auth-cm.yaml \
 && kubectl apply -f k8s-ingress/loadbalancer-aws-elb.yaml \
 && kubectl apply -f k8s-ingress/nginx-config-modified.yaml \
 && kubectl apply -f https://raw.githubusercontent.com/kubernetes/autoscaler/master/cluster-autoscaler/cloudprovider/aws/examples/cluster-autoscaler-autodiscover.yaml \
-&& kubectl annotate serviceaccount cluster-autoscaler -n kube-system eks.amazonaws.com/role-arn=$VALUE_2 \
+&& kubectl annotate serviceaccount cluster-autoscaler -n kube-system eks.amazonaws.com/role-arn=$2 \
 && kubectl patch deployment cluster-autoscaler -n kube-system -p '{"spec":{"template":{"metadata":{"annotations":{"cluster-autoscaler.kubernetes.io/safe-to-evict": "false"}}}}}' \
 && kubectl set image deployment cluster-autoscaler -n kube-system cluster-autoscaler=k8s.gcr.io/autoscaling/cluster-autoscaler:v1.21.0 \
 && kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml \
