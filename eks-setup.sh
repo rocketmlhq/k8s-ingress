5b76229c36d74c47be5e5b00cce4f900b59816c5
Mayank Kothari <mayankkthr@gmail.com>
2022-02-17 15:08:09 +0530

    commit

--- eks-setup.sh
+++ eks-setup.sh
@@ -5,7 +5,7 @@ curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/
 && mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin \
 && kubectl version --short --client && rm kubectl
 
-aws eks update-kubeconfig --region $1 --name $2 \
+aws eks update-kubeconfig --region $AWS_REGION --name $1 \
 && git clone https://github.com/mayankkthr/k8s-ingress.git \
 && kubectl apply -f k8s-ingress/ns-and-sa.yaml \
 && kubectl apply -f k8s-ingress/default-server-secret.yaml \
@@ -16,19 +16,19 @@ aws eks update-kubeconfig --region $1 --name $2 \
 && kubectl apply -f k8s-ingress/loadbalancer-aws-elb.yaml \
 && kubectl apply -f k8s-ingress/nginx-config-modified.yaml \
 && kubectl apply -f https://raw.githubusercontent.com/kubernetes/autoscaler/master/cluster-autoscaler/cloudprovider/aws/examples/cluster-autoscaler-autodiscover.yaml \
-&& kubectl annotate serviceaccount cluster-autoscaler -n kube-system eks.amazonaws.com/role-arn=$3 \
+&& kubectl annotate serviceaccount cluster-autoscaler -n kube-system eks.amazonaws.com/role-arn=$2 \
 && kubectl patch deployment cluster-autoscaler -n kube-system -p '{"spec":{"template":{"metadata":{"annotations":{"cluster-autoscaler.kubernetes.io/safe-to-evict": "false"}}}}}' \
 && kubectl set image deployment cluster-autoscaler -n kube-system cluster-autoscaler=k8s.gcr.io/autoscaling/cluster-autoscaler:v1.21.0 \
 && kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 
-sed -i 's/value1/$4/' k8s-ingress/aws-auth-cm.yaml
-sed -i 's/value2/$5/' k8s-ingress/aws-auth-cm.yaml
-sed -i 's/value3/$6/' k8s-ingress/aws-auth-cm.yaml
-sed -i 's/value4/$7/' k8s-ingress/aws-auth-cm.yaml
+sed -i 's/value1/$3/' k8s-ingress/aws-auth-cm.yaml
+sed -i 's/value2/$4/' k8s-ingress/aws-auth-cm.yaml
+sed -i 's/value3/$5/' k8s-ingress/aws-auth-cm.yaml
+sed -i 's/value4/$6/' k8s-ingress/aws-auth-cm.yaml
 
 kubectl apply -f k8s-ingress/aws-auth-cm.yaml
 
-sed -i 's/ClusterNameValue/$2/' k8s-ingress/cluster-autoscaler.yaml
+sed -i 's/ClusterNameValue/$1/' k8s-ingress/cluster-autoscaler.yaml
 
 kubectl apply -f k8s-ingress/cluster-autoscaler.yaml
 
