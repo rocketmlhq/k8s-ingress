9ba0d9e002817c1111ccb8a793cfc830c091d769
Mayank Kothari <mayankkthr@gmail.com>
2022-02-18 19:50:56 +0530

    commit

--- create-provider-and-role.sh
+++ create-provider-and-role.sh
@@ -4,7 +4,7 @@ curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/d
 && sudo mv /tmp/eksctl /usr/local/bin \
 && eksctl version
 
-aws configure set default.region $AWS_REGION
+aws configure set default.region $(curl http://169.254.169.254/latest/meta-data/placement/region)
 
 eksctl utils associate-iam-oidc-provider --cluster $1 --approve
 eksctl create iamserviceaccount --cluster=$1 --namespace=kube-system --name=cluster-autoscaler \
